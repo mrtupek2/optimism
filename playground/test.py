e271fba130a28359be201dd3e0a2a0f3bafee470
mrtupek <mrtupek@gmail.com>
2023-02-11 20:51:13 -0700

    Try making a graph

--- playground/test.py
+++ playground/test.py
@@ -7,7 +7,7 @@ x = [3, 4]
 print(x)
 
 #loop from 0 to 9
-def cbrt(i): 
+def cbrt(i):
     return np.cbrt(i*100)
 
 sum = 0
@@ -16,8 +16,7 @@ for i in range(10):
 
 #plot x^4 vs x
 x = np.linspace(-10, 10, 100)
-plt.plot(x, x**4)
-#color the line blue, with dashed line
+#plot the line with color blue and dashed line
 plt.plot(x, x**4, 'b--')
 #add a title
 plt.title('x^4')
@@ -46,3 +45,25 @@ def f(x):
 #print(grad_f(2.0))
 
 print(x)
+
+
+#compute the shortest path in a graph
+import networkx as nx
+G = nx.Graph()
+G.add_edge(1, 2, weight=4)
+G.add_edge(1, 3, weight=1)
+G.add_edge(2, 3, weight=1)
+G.add_edge(2, 4, weight=2)
+G.add_edge(3, 4, weight=5)
+G.add_edge(3, 5, weight=10)
+G.add_edge(4, 5, weight=1)
+path = nx.dijkstra_path(G, 1, 5)
+
+#plot the graph
+import matplotlib.pyplot as plt
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True)
+labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+plt.show()
+
